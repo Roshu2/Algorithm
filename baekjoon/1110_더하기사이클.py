45baf29d5f99d80f9ea5dde1624ba2b277a960f3
Roshu2 <kyumin1020@gmail.com>
2022-07-28 09:18:25 +0900

    ADD - day 3

--- baekjoon/1110_더하기사이클.py
+++ baekjoon/1110_더하기사이클.py
@@ -13,22 +13,3 @@ while True:
         break
 
 print(count)
-
-
-
-
-# def get_cycle(n):
-#     count = 0
-#     if n < 10:
-#         m = n * 10
-#         return get_cycle(m)
-#     else:
-#         sum_result = (n // 10) + (n % 10)
-#         new_num = (n % 10) * 10 + sum_result
-#         count += 1
-#         if new_num == n:
-#             print(count)
-#         else:
-#            return get_cycle(new_num)
-
-# get_cycle(n)
